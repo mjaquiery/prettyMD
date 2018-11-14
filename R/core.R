eab4535f9346a62a0986cbff887a1680a3588c8b
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-11-14 19:14:34 +0000

    initial commit
    * Initial commit of package designed to make test result printing easier

---
+++ R/core.R
@@ -0,0 +1,118 @@
+# Presentation functions --------------------------------------------------
+
+#' Format number to be a string beginning with a decimal point.
+#' @param num number to convert to string
+#' @param precision decimal places to preserve
+#' @param isProportion whether to strip leading 0 for 0.x values
+#' @param truncateZeros whether to strip trailing 0s
+#' @param ... discarded arguments used to allow overflowed calls from other functions
+#' @return \code{num} stripped of leading 0s and rounded to \code{precision} decimal places
+#'
+#' @examples
+#' data.frame(input = c(seq(-10,10),100), output = num2str(exp(c(seq(-10,10),100)), 4))
+#' data.frame(input = c(seq(-10,10),100), output = num2str(exp(c(seq(-10,10),100)), 4, isProportion = T))
+#'
+#' @export
+num2str <- function(num, precision = 2, isProportion = F, truncateZeros = F, ...) {
+  if(length(num) > 1)
+    return(sapply(num, function(x) num2str(num = x,
+                                           precision = precision,
+                                           isProportion = isProportion,
+                                           truncateZeros = truncateZeros)))
+  if(is.nan(num) | is.na(num))
+    return(as.character(num))
+  num <- round(num, precision)
+  # if we hit scientific notation then give up!
+  if(grepl('e', num, fixed = T))
+    return(as.character(num))
+  # leading 0 stripping
+  if(abs(num) < 1 & isProportion)
+    if(num == 0)
+      x <- '.'
+  else
+    x <- sub('^-?0\\.', ifelse(num < 0, '-.', '.'), as.character(num))
+  else
+    x <- as.character(num)
+  if(truncateZeros)
+    return(x)
+  # string manipulation to pad 0s
+  dot <- regexpr('.', x, fixed = T)
+  if(dot == -1) {
+    x <- paste0(x,'.')
+    dot <- regexpr('.', x, fixed = T)
+  }
+  right <- substr(x, dot, dot+precision) # portion of x after 0
+  right <- paste0(right, strrep('0',precision-nchar(right)+1))
+  x <- paste0(substr(x, 1, dot-1), right)
+  return(x)
+}
+
+#' Wrapper for \code{num2str(..., isProportion = T)}
+#' @inheritDotParams num2str
+#'
+#'@examples
+#' data.frame(input = c(seq(-10,10),100),
+#'   num = num2str(exp(c(seq(-10,10),100)), 4),
+#'   prop = prop2str(exp(c(seq(-10,10),100)), 4)
+#' )
+#'
+#' @export
+prop2str <- function(...) {
+  return(num2str(num = proportion, precision = precision, isProportion = T, truncateZeros = truncateZeros))
+}
+
+#' Print the mean and CIs of a vector
+#' @param vector data in
+#' @param label markdown prefix for the stats
+#' @param conf.int width of the confidence intervals, NA to suppress
+#' @param na.rm whether NA values are removed before averaging
+#' @param decimals decimal places to round to
+#' @param isProportion whether to print the values as proportions (strip leading 0)
+#' @param showRange whether to include the range of the data
+#' @return string representation of the mean, CIs, and range of the \code{vector}
+#'
+#' @examples
+#' md.mean(rnorm(1000), label = '*M*|random')
+#' md.mean(rnorm(1000), label = '*M*|random', conf.int = NA) # no longer needs lsr package
+#' md.mean(runif(1000), label = 'Mean probability', isProportion = T, showRange = T)
+#'
+#' @export
+md.mean <- function(vector, label = '*M*', decimals = 2, na.rm = F, conf.int = .95, isProportion = F, showRange = F) {
+  mu <- mean(vector, na.rm = na.rm)
+  out <- paste0(label,' = ', num2str(mu,decimals, isProportion = isProportion))
+
+  if(!is.na(conf.int)) {
+     if (!requireNamespace("lsr", quietly = TRUE)) {
+      stop("Package \"lsr\" needed to calculate confidence intervals. Please install it or use md.mean(..., conf.int = NA).",
+           call. = FALSE)
+    }
+    ci <- lsr::ciMean(vector, conf = conf.int, na.rm = na.rm)
+    ci.low <- ci[1]
+    ci.high <- ci[2]
+    out <- paste0(out, ' [', num2str(ci.low,decimals), ', ', num2str(ci.high,decimals),']')
+  }
+  if(showRange) {
+    r <- num2str(range(vector, na.rm = na.rm), decimals, isProportion = isProportion)
+    out <- paste0(out, ' range = (',
+                  r[[1]], ', ', r[[2]], ')')
+  }
+
+  return(out)
+}
+
+#' Extract the Bayes Factor from a Bayesian test
+#' @param bayesTest the test whose parameter will be extracted
+#' @inheritDotParams num2str
+#'
+#' @examples
+#' \dontrun{
+#'
+#' data <- data.frame(x = rnorm(100), y = rnorm(100, 0.2)) # two normal distributions with some overlap
+#' md.BF(BayesFactor::ttestBF(data$x, data$y, paired = T))
+#' }
+#'
+#' @export
+md.BF <- function(bayesTest, ...) {
+  out <- paste(name, '=', num2str(exp(bayesTest@bayesFactor$bf)), ...)
+  return(out)
+}
---
+++ R/ttest.R
@@ -0,0 +1,79 @@
+
+# T-test functions --------------------------------------------------------
+
+#' Appropriately formatted t-test output
+#' @param result t.test output to format
+#' @param decimals number of decimal places for means
+#' @param decimals.p number of decimal places for p-values, defaults to
+#'   \code{decimals}+1
+#'
+#' @examples
+#' data <- data.frame(x = rnorm(100), y = rnorm(100, 0.2)) # two normal distributions with some overlap
+#' md.t(t.test(data$x, data$y, paired = T), decimals = 3)
+#'
+#' @export
+md.t <- function(result, decimals = 2, decimals.p = NULL) {
+  if(is.null(decimals.p))
+    decimals.p <- decimals + 1
+  df <- num2str(result$parameter, decimals, truncateZeros = T)
+  stat <- num2str(result$statistic, decimals)
+  p <- ifelse(result$p.value < 10^-decimals.p,
+              paste0('< .', strrep('0', decimals.p-1), '1'),
+              paste0(' = ', num2str(result$p.value, decimals.p, isProportion = T)))
+  out <- paste0('*t*(', df, ') = ', stat,
+                ', *p* ', p)
+  return(out)
+}
+
+#' Run a t-test and return neatly formatted output
+#' \code{x}, and either \code{y} or \code{mu} must be specified.
+#' @param x vector for testing
+#' @param y additional vector for testing. If singular, functions as \code{mu}
+#' @param labels vector of labels for x and y
+#' @param mu value against which x will be tested
+#' @param paired whether observations are paired (e.g. repeated measures)
+#' @inheritDotParams md.mean
+#'
+#' @examples
+#' data <- data.frame(x = rnorm(100), y = rnorm(100, 0.2)) # two normal distributions with some overlap
+#' md.ttest(data$x, data$y, c('*Mean|Control*', '*Mean|Treatment*'), paired = T)
+#'
+#' @return formatted string like t(df) = t.value, p < .05, d = 0.23, BF = 4.50; M1 = 3.05 [1.23, 4.55], M2 = 5.12 [4.20, 5.99]
+#'
+#' @export
+md.ttest <- function (x, y = NULL, labels = NULL, mu = NULL, paired = F, ...) {
+  pkgs <- list()
+  for(pkg in c('lsr', 'BayesFactor'))
+    pkgs[[pkg]] <- requireNamespace(pkg, quietly = T)
+
+  oneSided <- (length(y)==1 | !is.null(mu))
+  if(is.null(labels)) {
+    if(oneSided)
+      labels <- '*M*'
+    else
+      labels <- c('*M1*', '*M2*')
+  }
+  # support for one-sample tests
+  if(oneSided) {
+    y = ifelse(is.null(mu), y, mu)
+    .t <- md.t(t.test(x, mu = y, paired = paired))
+    .d <- ifelse(pkgs[['lsr']],
+                 paste(', *d* =', num2str(lsr::cohensD(x, mu = y), ...)),
+                 NULL)
+    .bf <- ifelse(pkgs[['BayesFactor']],
+                  paste(',', md.BF(BayesFactor::ttestBF(x, mu = y, paired = paired), ...)),
+                  NULL)
+    means <- paste0('; ', md.mean(x, label = labels[1], ...), ', mu = ', y)
+  } else {
+    .t <- t.test(x, y, paired = paired)
+    .d <- ifelse(pkgs[['lsr']],
+                 paste(', *d* =', num2str(lsr::cohensD(x, y), ...)),
+                 NULL)
+    .bf <- ifelse(pkgs[['BayesFactor']],
+                  paste(',', md.BF(BayesFactor::ttestBF(x, y, paired = paired), ...)),
+                  NULL)
+    means <- paste0('; ', md.mean(x, label = labels[1], ...), ', ',
+                    md.mean(y, label = labels[2], ...))
+  }
+  paste0(.t, .d, .bf, means)
+}
